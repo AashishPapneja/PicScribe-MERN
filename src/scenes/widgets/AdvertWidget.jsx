d2fb69e99995ce5d027c4a16ff8aa46185923462
AashishPapneja <96795926+AashishPapneja@users.noreply.github.com>
2023-07-16 19:34:22 +0530

    Update AdvertWidget.jsx

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/PicScribe_LOGO.png"
+        src="https://picscribe-backend3.onrender.com/assets/PicScribe_LOGO.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
